2d2b0cce707e27ce69839f9ee55be4edf9e5a924
Thomas Papp <pappmthomas@gmail.com>
2021-10-22 18:24:53 -0500

    fixed up wallet selection component

--- src/components/WalletSelection.tsx
+++ src/components/WalletSelection.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Button, Grid, makeStyles, Paper, Typography } from '@material-ui/core'
+import { Button, Grid, makeStyles, Typography } from '@material-ui/core'
 import wallets, { Wallet } from "../wallets/wallets"
 
 const useStyles = makeStyles(() => ({
@@ -15,22 +15,20 @@ const WalletSelection = (props: { connect: (wallet: Wallet) => Promise<void> }):
         <Grid container justifyContent="center" alignItems="center" direction="column" spacing={2}>
             {
                 wallets.map(wallet => (
-                    <Grid item key={wallet.name}>
-                        <Button variant="text" onClick={async () => await props.connect(wallet)}>
-                            <Grid container justifyContent="center" alignItems="center" direction="column" spacing={2}>
-                                <Grid item>
-                                    <img className={classes.walletLogo} src={wallet.icon} alt={`${wallet.name}_icon`} />
-                                </Grid>
-
-                                <Grid item>
-                                    <Typography variant="h4">{wallet.name}</Typography>
-                                </Grid>
-
-                                <Grid item>
-                                    <Typography color="textSecondary">{wallet.desc}</Typography>
-                                </Grid>
+                    <Grid item key={wallet.name} component={Button} onClick={async () => await props.connect(wallet)}>
+                        <Grid container justifyContent="center" alignItems="center" direction="column" spacing={2}>
+                            <Grid item>
+                                <img className={classes.walletLogo} src={wallet.icon} alt={`${wallet.name}_icon`} />
                             </Grid>
-                        </Button>
+
+                            <Grid item>
+                                <Typography variant="h4">{wallet.name}</Typography>
+                            </Grid>
+
+                            <Grid item>
+                                <Typography color="textSecondary">{wallet.desc}</Typography>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 ))
             }
